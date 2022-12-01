fdf5159e422e25b99b931cd5f952e6ba233d5611
matteospatola <matteospatola@hotmail.com>
2022-11-30 20:11:34 -0500

    Initial Commit

---
+++ twitter.py
@@ -0,0 +1,26 @@
+import tweepy
+import keys
+
+
+def api():
+    auth = tweepy.OAuthHandler(keys.api_key, keys.api_key_secret)
+    auth.set_access_token(keys.access_token, keys.access_token_secret)
+
+    return tweepy.API(auth)
+
+
+def tweet(api: tweepy.API, message: str, image_path=None):
+    if image_path:
+        api.update_status_with_media(message, image_path)
+    else:
+        api.update_status(message)
+
+    print("Tweet was successful!")
+
+
+if __name__ == "__main__":
+    api = api()
+    tweet(api, "Pat is cringe")
+
+
+
---
+++ weather.py
@@ -0,0 +1,54 @@
+import datetime
+import requests
+import weatherkey
+import json
+
+
+
+def get_weather(api_key, city_name):
+    url =f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"
+    return requests.get(url).json()
+    #print(response)
+    #print(json.dumps(response, indent=2)
+
+
+
+def org_data():
+    data = get_weather(weatherkey.api_key, input("What is your location: "))
+    keys = ['weather', 'main', 'wind']
+    values = []
+    for k in data:
+        if k in keys:
+            values.append(data[k])
+
+
+print(org_data())
+
+
+
+
+
+
+
+
+
+
+
+
+
+org_data()
+
+
+
+
+
+
+
+print(org_data())
+
+
+
+
+
+
+
